8a0126275b57d1f20d796007388034f3ee10223a
ammon-m <106837909+ammon-m@users.noreply.github.com>
2024-06-03 12:15:40 -0700

    scrollbar fix?

--- pages/terminal2/src/main.js
+++ pages/terminal2/src/main.js
@@ -399,7 +399,7 @@ function drawCanvas()
     const totalLines = terminal._rawText.split("\n").length;
 
     cursorCtx.fillStyle = theme.foreground
-    cursorCtx.fillRect(cursorCtx.canvas.width - charWidth, (terminal._scroll / (totalLines + 2)) * cursorCtx.canvas.height, charWidth, ((maxRows + 3/lineHeight) / (totalLines + 2) + 3/lineHeight) * cursorCtx.canvas.height)
+    cursorCtx.fillRect(cursorCtx.canvas.width - charWidth, (terminal._scroll / (totalLines + 2)) * cursorCtx.canvas.height, charWidth, ((maxRows + 3/lineHeight) / (totalLines + Math.floor(cursorCtx.canvas.height / lineHeight)) + 3/lineHeight) * cursorCtx.canvas.height)
 }
 
 init("hello world")
