8e0ffe0cc3461ddda1bf956dbdbfc3ca11342737
ammon-m <106837909+ammon-m@users.noreply.github.com>
2024-06-03 13:27:15 -0700

    yo

--- pages/terminal2/src/main.js
+++ pages/terminal2/src/main.js
@@ -403,7 +403,7 @@ function drawCanvas()
         cursorCtx.canvas.width - charWidth,
         (Math.floor(terminal._scroll) / (totalLines + maxRows)) * cursorCtx.canvas.height,
         charWidth,
-        (maxRows / (totalLines + maxRows)) * cursorCtx.canvas.height
+        (maxRows / (totalLines + maxRows - 1)) * cursorCtx.canvas.height
     )
 }
 
