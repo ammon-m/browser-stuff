a6c0fc6bb79fc2f4739b7819847683e10d60101e
ammon-m <106837909+ammon-m@users.noreply.github.com>
2024-06-04 13:35:00 -0700

    cut copy paste mini-tweak

--- pages/terminal2/src/main.js
+++ pages/terminal2/src/main.js
@@ -77,9 +77,9 @@ contextMenuElement.style.left = "0";
 contextMenuElement.style.top = "0";
 
 const contextMenuItems = {
-    cCut:   (event) => { consoleFocused = true; copy(event, true, true); ctxMenuKillable = true; contextMenuElement.classList.add("hidden"); },
-    cCopy:  (event) => { consoleFocused = true; copy(event, true);       ctxMenuKillable = true; contextMenuElement.classList.add("hidden"); },
-    cPaste: (event) => { consoleFocused = true; paste(event, true);      ctxMenuKillable = true; contextMenuElement.classList.add("hidden"); },
+    cCut:   (event) => { copy(event, true, true); consoleFocused = true; ctxMenuKillable = true; contextMenuElement.classList.add("hidden"); },
+    cCopy:  (event) => { copy(event, true);       consoleFocused = true; ctxMenuKillable = true; contextMenuElement.classList.add("hidden"); },
+    cPaste: (event) => { paste(event, true);      consoleFocused = true; ctxMenuKillable = true; contextMenuElement.classList.add("hidden"); },
     dTest: (event) => {},
 }
 
